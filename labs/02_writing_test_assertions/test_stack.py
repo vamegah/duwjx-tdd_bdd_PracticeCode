9ebaf7aab91491b4499afafbcdef7fb080a10dba
vamegah <makvim@yahoo.com>
2024-12-31 16:32:12 -0500

    Implement test cases for stack operations in test_stack.py

--- labs/02_writing_test_assertions/test_stack.py
+++ labs/02_writing_test_assertions/test_stack.py
@@ -14,16 +14,28 @@ class TestStack(TestCase):
 
     def test_push(self):
         """Test pushing an item into the stack"""
-        raise Exception("not implemented")
+        self.stack.push(3)
+        self.assertEqual(self.stack.peek(), 3)
+        self.stack.push(5)
+        self.assertEqual(self.stack.peek(), 5)
 
     def test_pop(self):
         """Test popping an item of off the stack"""
-        raise Exception("not implemented")
+        self.stack.push(3)
+        self.stack.push(5)
+        self.assertEqual(self.stack.pop(), 5)
+        self.assertEqual(self.stack.peek(), 3)
+        self.stack.pop()
+        self.assertTrue(self.stack.is_empty())
 
     def test_peek(self):
         """Test peeking at the top the stack"""
-        raise Exception("not implemented")
+        self.stack.push(3)
+        self.stack.push(5)
+        self.assertEqual(self.stack.peek(), 5)
 
     def test_is_empty(self):
         """Test if the stack is empty"""
-        raise Exception("not implemented")
+        self.assertTrue(self.stack.is_empty())
+        self.stack.push(5)
+        self.assertFalse(self.stack.is_empty())
