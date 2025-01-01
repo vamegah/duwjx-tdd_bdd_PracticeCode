17770dfc60bb1733084c4301d2a548da739de431
vamegah <makvim@yahoo.com>
2024-12-31 21:56:40 -0500

    Setup environment for test

--- labs/08_environment_setup/features/environment.py
+++ labs/08_environment_setup/features/environment.py
@@ -5,8 +5,14 @@ from os import getenv
 from selenium import webdriver
 
 
+BASE_URL = getenv('BASE_URL', 'http://localhost:8080')
+WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
+
 def before_all(context):
     """ Executed once before all tests """
+    context.base_url = BASE_URL
+    context.wait_seconds = WAIT_SECONDS
+
 
 
 def after_all(context):
